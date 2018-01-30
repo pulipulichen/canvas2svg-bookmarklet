f11de2b4bfa2d69fca755c60d5068f1af2e58118
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-01-30 20:07:29 +0800

    0130 好了

--- canvas2svg-bookmarklet.js
+++ canvas2svg-bookmarklet.js
@@ -1,1 +1,1 @@
-function(e,s){e.src=s;e.onload=function(){_main();};document.head.appendChild(e);})(document.createElement('script'),'https://pulipulichen.github.io/canvas2svg-bookmarklet/canvas2svg');
+(function(e,s){e.src=s;e.onload=function(){_main();};document.head.appendChild(e);})(document.createElement('script'),'https://pulipulichen.github.io/canvas2svg-bookmarklet/canvas2svg.js');
--- canvas2svg.js
+++ canvas2svg.js
@@ -59,6 +59,78 @@ var _convert_canvas_to_svg = function (_callback) {
 
 _load_filesaver(function () {
     _load_canvassvg(function () {
-        _convert_canvas_to_svg();
-    });
-});
+       var canvas2svg_is_ready = false;
+
+
+
+var _load_filesaver = function (_callback) {
+    if (typeof(saveAs) === "function") {
+		return _callback();
+	}
+	else {
+		$.getScript("https://pulipulichen.github.io/canvas2svg-bookmarklet/lib/FileSaver.min.js", function () {
+			_callback();
+		});
+	}
+};
+
+var _load_canvassvg = function (_callback) {
+    if (typeof(CanvasSVG) !== "function") {
+		return _callback();
+	}
+	else {
+		$.getScript("https://pulipulichen.github.io/canvas2svg-bookmarklet/lib/canvas-getsvg.js", function () {
+			_callback();
+		});
+	}
+};
+
+var _convert_canvas_to_svg = function (_callback) {
+	
+	var _canvas_coll = $('canvas:visible');
+	
+	var _next = function (_i) {
+		_i++;
+		setTimeout(function () {
+			_loop(_i);
+		}, 500);
+	};
+	
+	var _loop = function (_i) {
+		if (_i < _canvas_coll.length) {
+			var _canvas = _canvas_coll.eq(_i);
+			
+			if (typeof(_canvas.attr("id")) === "undefined") {
+				_canvas.attr("id", "canvas_" + _i);
+			}
+			var _id = _canvas.attr("id");
+			
+			var cs = new CanvasSVG.Deferred();
+			var canvas = document.getElementById(_id);
+			cs.wrapCanvas(canvas);
+			var ctx = canvas.getContext('2d');
+			
+			var svg = new Blob([cs.getSVG().outerHTML], {type: 'text/plain'});
+			saveAs(svg, _id + ".svg");
+			
+			next(_i);
+		}
+		else {
+			if (typeof(_callback) === "function") {
+				_callback();
+			}
+		}
+	};
+	
+	_loop(0);
+};
+
+var _main = function () {
+	_load_filesaver(function () {
+		_load_canvassvg(function () {
+			_convert_canvas_to_svg();
+		});
+	});	
+};
+
+function(e,s){e.src=s;e.onload=function(){jQuery.noConflict();$=jQuery;_main();};document.head.appendChild(e);})(document.createElement('script'),'https://code.jquery.com/jquery-latest.min.js');
